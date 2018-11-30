3c7f4f4649ce7918e460dd674070bf32762c5539
Franklin Antony <franklinanton.007@gmail.com>
2018-12-01 00:39:44 +0530

    :zap: Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Clean Your Mac Bash Script
+
+[![Bash Shell](https://badges.frapsoft.com/bash/v1/bash.png?v=103)](#) [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](#) [![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.svg?v=102)](#) [![Open Source Love](https://badges.frapsoft.com/os/mit/mit.svg?v=102)](#)
+
+This bash script was primarily built to clean up the chunks of files that has been clogging your Mac's memory.
+
+## Script Usage
+
+---
+
+## Credits
+
+---
+
+This script was primarily developed based on the stackoverflow posts [[1](https://stackoverflow.com/questions/7688614/osx-shell-script-deleting-all-desktop-files)],[[2](https://stackoverflow.com/questions/51718727/mac-shell-script-to-delete-files)] and this [github project](https://raw.githubusercontent.com/mengfeng/clean-my-mac/master/clean_my_mac.sh).
---
+++ script.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+echo "Running Clean Your Mac Script 💛"
+
+
+#Updating brew
+echo "Updating Brew 🍺 and Cleaning up the Cask 🌊 ........"
+
+brew update
+brew cask update
+brew upgrade
+brew cleanup
+brew cask cleanup
+brew doctor 
+
+
+#user cache file
+echo "Cleaning user cache file from ~/Library/Caches"
+sudo rm -rf ~/Library/Caches/*
+echo "✅ Done Cleaning from ~/Library/Caches"
+#user logs
+echo "Cleaning user log file from ~/Library/logs"
+sudo rm -rf ~/Library/logs/*
+echo "✅ Done Cleaning from ~/Library/logs"
+#user preference log
+echo "Cleaning user preference logs"
+#rm -rf ~/Library/Preferences/*
+echo "✅ Done Cleaning from /Library/Preferences"
+#system caches
+echo "Cleaning system caches"
+sudo rm -rf /Library/Caches/*
+echo "✅ Done Cleaning system cache"
+#system logs
+echo "Cleaning system logs from /Library/logs"
+sudo rm -rf /Library/logs/*
+echo "✅ Done Cleaning from /Library/logs"
+echo "Cleaning system logs from /var/log"
+sudo rm -rf /var/log/*
+echo "✅ Done Cleaning from /var/log"
+echo "Cleaning from /private/var/folders"
+sudo rm -rf /private/var/folders/*
+echo "✅ Done Cleaning from /private/var/folders"
+#ios photo caches
+echo "Cleaning ios photo caches"
+rm -rf ~/Pictures/iPhoto\ Library/iPod\ Photo\ Cache/*
+echo "✅ Done Cleaning from ~/Pictures/iPhoto Library/iPod Photo Cache"
+#application caches
+echo "Cleaning application caches"
+for x in $(ls ~/Library/Containers/) 
+do 
+    echo "Cleaning ~/Library/Containers/$x/Data/Library/Caches/"
+    rm -rf ~/Library/Containers/$x/Data/Library/Caches/*
+    echo "✅ Done Cleaning ~/Library/Containers/$x/Data/Library/Caches"
+done
+echo "✅ Done Cleaning for application caches"
